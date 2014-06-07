88f23f26878683d81141edf6fe347d2f55accf5c
Nicolas DI PRIMA <nicolas@di-prima.fr>
2014-06-07 14:06:04 +0200

    implement loop to push packed until all the data isn't flush

--- Data/Packer/Incremental/Packing.hs
+++ Data/Packer/Incremental/Packing.hs
@@ -26,13 +26,13 @@ import Data.Word
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Internal as B
 
-type Pusher = B.ByteString -> IO ()
+type PackingPusher = B.ByteString -> IO ()
 
 data PackedBuffer = PackedBuffer Int (ForeignPtr Word8)
 
 data PackingIncremental a = PackingIncremental
     { runPI_ :: PackedBuffer
-             -> Pusher
+             -> PackingPusher
              -> Memory
              -> IO (a, PackedBuffer, Memory) }
 
@@ -79,15 +79,24 @@ packIncrementalCheckAct n act = PackingIncremental exPackIncr
                   tFptr <- B.mallocByteString n
                   r <- withForeignPtr tFptr act
 
-                  tmpPtr <- withForeignPtr tFptr $ \tptr -> B.memcpy ptr tptr sz >> return (tptr `plusPtr` sz)
-                  pusher (B.PS fPtr 0 size)
-                  -- TODO: loop until n > size
+                  (npb, nMem) <- withForeignPtr tFptr $ \tptr -> pushAll (PackedBuffer size fPtr) pusher (Memory ptr sz) tptr n
 
-                  -- Create a new Foreign Ptr
-                  nFptr <- B.mallocByteString size
-                  nptr <- withForeignPtr nFptr $ \nptr -> B.memcpy nptr tmpPtr (size - sz) >> return (nptr `plusPtr` sz)
                   -- Create tmp ByteString to flush the content of the
-                  return (r, PackedBuffer size nFptr, Memory nptr (size - sz))
+                  return (r, npb, nMem)
               | otherwise = do
                   r <- act ptr
                   return (r, PackedBuffer size fPtr, Memory (ptr `plusPtr` n) (n - sz))
+          pushAll :: PackedBuffer -> PackingPusher -> Memory -> Ptr Word8 -> Int -> IO (PackedBuffer, Memory)
+          pushAll (PackedBuffer pbSize pbFptr) pusher (Memory ptr mLeft) tmpPtr size =
+              case compare size mLeft of
+                  LT -> do B.memcpy ptr tmpPtr size
+                           return (PackedBuffer pbSize pbFptr, Memory (ptr `plusPtr` size) (mLeft - size))
+                  EQ -> do B.memcpy ptr tmpPtr size
+                           pusher (B.PS pbFptr 0 pbSize)
+                           nFptr <- B.mallocByteString pbSize
+                           withForeignPtr nFptr $ \nptr -> return (PackedBuffer pbSize nFptr, Memory nptr pbSize)
+                  GT -> do B.memcpy ptr tmpPtr mLeft
+                           pusher (B.PS pbFptr 0 pbSize)
+                           nFptr <- B.mallocByteString pbSize
+                           withForeignPtr nFptr $ \nptr ->
+                               pushAll (PackedBuffer pbSize nFptr) pusher (Memory nptr pbSize) tmpPtr (size - mLeft)
